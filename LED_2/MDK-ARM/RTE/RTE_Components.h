34b1ec2f3b43a0c2113ce7d221b307124e68a4b7
LMN <lmnscut@gmail.com>
2026-01-02 14:10:14 +0800

    增加了LED和蜂鸣器练习

---
+++ LED_2/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LED_2' 
+ * Target:  'LED_2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
